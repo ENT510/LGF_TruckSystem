3b48e5f0cff07a832e4496e016665ed506916578
ENT510 <145626625+ENT510@users.noreply.github.com>
2024-12-18 18:35:37 +0100

    Update logistic.sql

--- logistic.sql
+++ logistic.sql
@@ -8,3 +8,14 @@ CREATE TABLE IF NOT EXISTS `lgf_logistic` (
   `reward_redeemed` longtext DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+
+-- Dump della struttura di tabella apocalypse.lgf_alldeliveries
+CREATE TABLE IF NOT EXISTS `lgf_alldeliveries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ZoneName` varchar(255) NOT NULL,
+  `AllDeliveries` longtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
